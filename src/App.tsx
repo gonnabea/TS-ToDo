ddda96fa7f9b09e15f514acad85d914720a1c6cf
Choi Jiwon <winning1993@naver.com>
2020-09-09 02:23:20 +0900

    Creating a Todo List App in React using Typescript (Part 3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { TodoList } from "./TodoList"
+import { AddTodoForm } from "./AddTodoForm"
 
 const initialTodos: Array<Todo> = [
   { text: "Walk the dog", complete: true },
@@ -22,9 +23,14 @@ const App: React.FC = () => {
     setTodos(newTodos)
   }
 
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trim() !== "" && setTodos([...todos, { text: newTodo, complete: false }])
+  }
+
   return (
     <div>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
     </div>
   )
 }
