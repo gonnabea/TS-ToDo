fbd287ca2cb72b35a511fc2c7bae01fbf02f394a
Choi Jiwon <winning1993@naver.com>
2020-09-08 14:22:14 +0900

    Creating a Todo List App in React using Typescript (Part 2)

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -2,12 +2,13 @@ import React from "react"
 
 interface TodoListItemProps {
   todo: Todo
+  toggleTodo: ToggleTodo
 }
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => (
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleTodo }) => (
   <li>
     <label style={{ textDecoration: todo.complete ? "line-through" : "none" }}>
-      <input type="checkbox" checked={todo.complete} />
+      <input type="checkbox" checked={todo.complete} onChange={() => toggleTodo(todo)} />
       {todo.text}
     </label>
   </li>
